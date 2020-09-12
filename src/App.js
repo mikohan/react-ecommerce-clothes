1e36d6dca3269b4e8e57cb4be734747cf27b6811
mikohan <angara99@gmail.com>
2020-09-12 20:00:37 +0300

    Created Basic Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
@@ -13,6 +14,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />.
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route exact={true} path="/shop" component={ShopPage} />
