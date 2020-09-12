f3d6add5ba2203bae30a48ff8e6727750b417af3
mikohan <angara99@gmail.com>
2020-09-12 19:30:10 +0300

    Created collection prewiew component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
 const HatsPage = () => (
@@ -14,6 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
+        <Route exact={true} path="/shop" component={ShopPage} />
         <Route exact={true} path="/shop/hats" component={HatsPage} />
       </Switch>
     </div>
